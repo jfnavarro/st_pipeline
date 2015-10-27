Added saturation point computation, fixed bug in hierarchical clustering, minor improvements
Change-Id: I26f863679b39bccc418c7f5c394ec475fdd155e3

pipeline/scripts/json2scatter.py
@@ -16,20 +16,93 @@ import os
 import re
 import matplotlib
 matplotlib.use('Agg')
+from matplotlib import transforms
 from matplotlib import cm
 from matplotlib.colors import LinearSegmentedColormap
 from matplotlib import pyplot as plt
 from stpipeline.common.json_utils import json_iterator
-from stpipeline.common.json_utils import write_json
 import numpy as np
 
 blues = LinearSegmentedColormap.from_list('blues', [[0.02, 0.44, 0.69, 0.3],
                                                     [0.02, 0.44, 0.69, 1.0]])
 
-def main(json_file, highlight_regexes, image, only_highlight, cutoff):
+
+def parseJSON(input_data, cutoff, highlight_regexes, highlights, expression):
+    
+    it = json_iterator(input_data)
+    for doc in it:   
+        
+        if cutoff is not None and int(doc['hits']) < cutoff:
+            continue
+        
+        expression[doc['x'], doc['y']] += doc['hits']
+        
+        if not highlight_regexes:
+            continue
+        
+        for i, regex in enumerate(highlight_regexes):
+            if re.search(regex, doc["gene"]):
+                highlights[i].add((doc['x'], doc['y']))
+                
+    return highlights, expression, colors
+    
+def parseCSV(input_data, cutoff, highlight_regexes, highlights, expression):
+    
+    with open(input_data,"r") as filehandler_read:
+        for line in filehandler_read.readlines():
+            if line.find("#") != -1:
+                continue
+            tokens = line.split()
+            hits = int(tokens[4])
+            gene = tokens[0]
+            x = int(tokens[2])
+            y = int(tokens[3])
+            bc = tokens[1]
+            
+            if cutoff is not None and hits < cutoff:
+                continue
+            
+            expression[x, y] += hits
+            
+            if not highlight_regexes:
+                continue
+            
+            for i, regex in enumerate(highlight_regexes):
+                if re.search(regex, gene):
+                    highlights[i].add((x, y))
+                    
+    return highlights, expression
+
+def main(input_data, highlight_regexes, image, only_highlight, cutoff, highlight_barcodes, alignment):
     fig = []
     ax = []
     highlights = []
+    expression = np.zeros((33, 33), dtype=np.int)
+    colors = np.zeros((33,33), dtype=np.int)
+    alignment_matrix = np.zeros((3,3), dtype=np.float)
+
+    if alignment is not None:
+        alignment_matrix[0,0] = alignment[0]
+        alignment_matrix[0,1] = alignment[1]
+        alignment_matrix[0,2] = alignment[2]
+        alignment_matrix[1,0] = alignment[3]
+        alignment_matrix[1,1] = alignment[4]
+        alignment_matrix[1,2] = alignment[5]
+        alignment_matrix[2,0] = alignment[6]
+        alignment_matrix[2,1] = alignment[7]
+        alignment_matrix[2,2] = alignment[8]
+        
+    # Parse the clusters colors if needed
+    if highlight_barcodes is not None:        
+        with open(highlight_barcodes, "r") as filehandler_read:
+            for line in filehandler_read.readlines():
+                tokens = line.split()
+                barcode = tokens[1]
+                cluster = int(tokens[0])
+                x = int(tokens[2])
+                y = int(tokens[3])
+                colors[x,y] = cluster
+
     for _ in highlight_regexes if highlight_regexes else [0]:
         f = plt.figure()
         a = f.add_subplot(111, aspect='equal')
@@ -37,58 +110,54 @@ def main(json_file, highlight_regexes, image, only_highlight, cutoff):
         ax.append(a)
         highlights.append(set([]))
 
-    expression = np.zeros((1000, 1000), dtype=np.int)
-
-    it = json_iterator(json_file)
-    for doc in it:
-        
-        if cutoff is not None and int(doc['hits']) < cutoff:
-            continue
-        
-        expression[doc['x'], doc['y']] += doc['hits']
-
-        if not highlight_regexes:
-            continue
-
-        for i, regex in enumerate(highlight_regexes):
-            if re.search(regex, doc["gene"]):
-                highlights[i].add((doc['x'], doc['y']))
-
+    if input_data.endswith(".json"):
+        highlights,expression = parseJSON(input_data, cutoff,
+                                          highlight_regexes, highlights, expression)
+    else:
+        highlights,expression = parseCSV(input_data, cutoff, 
+                                         highlight_regexes, highlights, expression)
+     
     x, y = expression.nonzero()
 
-    cmap = blues
     if image:
-        img = np.load(image)
-        img_edge = np.nonzero(img)[0].min()
-        cmap = cm.Blues_r
+        img = plt.imread(image)
 
     for a in ax:
         if not only_highlight:
-            a.scatter(x, y, c=expression[x, y],
-                      edgecolor="none",
-                      s=10,
-                      label="Expression",
-                      cmap=cmap,
-                      )
+            base_trans = a.transData 
+            tr = transforms.Affine2D(matrix = alignment_matrix) + base_trans 
 
+            #a.scatter(x, y, c=expression[x, y],
+            #          edgecolor="none",
+            #          s=30,
+            #          label="Expression",
+            #          transform = tr
+            #          )
+            
+            if highlight_barcodes is not None:
+                x2, y2 = colors.nonzero()
+                a.scatter(x2, y2, c=colors[x2, y2],
+                          edgecolor="none",
+                          s=30,
+                          label="Cluster",
+                          transform = tr
+                          )
+                #a.colorbar()
         if image:
             a.imshow(img)
 
     for i, regex in enumerate(highlight_regexes if highlight_regexes else [0]):
+        
         if len(highlights[i]) == 0:
             continue
 
         x, y = zip(*highlights[i])
         ax[i].scatter(x, y, c="#CA0020",
                       edgecolor="#CA0020",
-                      s=20,
+                      s=40,
                       label=highlight_regexes[i])
 
     for i, a in enumerate(ax):
-        if not image:
-            a.invert_yaxis()
-        else:
-            a.set_ybound(lower=img_edge)
 
         a.set_xlabel("X")
         a.set_ylabel("Y")
@@ -100,18 +169,18 @@ def main(json_file, highlight_regexes, image, only_highlight, cutoff):
         else:
             ending = ".png"
 
-        img_file = os.path.basename(json_file).replace(".json", ending)
+        img_file = "data_plot" + ending
         if image:
             fig[i].set_size_inches(16, 16)
         else:
             fig[i].set_size_inches(10, 8)
 
-        fig[i].savefig(img_file)
+        fig[i].savefig(img_file, dpi=900)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument("json_file", help="JSON ST-data file")
+    parser.add_argument("input_data", help="JSON ST-data file or CSV ST-data file")
     parser.add_argument("--highlight", help="Regular expression for \
                         gene symbols to highlight in the quality \
                         scatter plot. Can be given several times.",
@@ -119,9 +188,15 @@ if __name__ == '__main__':
                         type=str,
                         action='append')
     parser.add_argument("--image", default=None)
-    parser.add_argument("--only-highlight", default=False, action='store_true')
-    parser.add_argument("--cutoff", type=int, default=None)
-    
+    parser.add_argument("--only-highlight", help="Only shows highlighted genes",
+                        default=False, action='store_true')
+    parser.add_argument("--cutoff", help="Do not include genes below this reads cut off",
+                        type=int, default=None)
+    parser.add_argument("--highlight-barcodes", default=None,
+                        help="File with a list of barcodes in a column and a list of clusters in the other column")
+    parser.add_argument("--alignment", help="Aligment matrix needed when using the image", nargs="+", type=float, default=None)
     args = parser.parse_args()
 
-    main(args.json_file, args.highlight, args.image, args.only_highlight, args.cutoff)
+    main(args.input_data, args.highlight, args.image, 
+         args.only_highlight, args.cutoff, 
+         args.highlight_barcodes, args.alignment)

pipeline/stpipeline/common/clustering.py
@@ -5,7 +5,7 @@ by their molecular barcodes using different approaches
 """
 
 import numpy as np
-import scipy.cluster.hierarchy
+from scipy.cluster.hierarchy import linkage,fcluster
 import counttrie.counttrie as ct
 from collections import defaultdict
 from stpipeline.common.distance import hamming_distance
@@ -44,7 +44,7 @@ def extractMolecularBarcodes(reads, mc_start_position, mc_end_position):
     return sorted(molecular_barcodes_count_list, key=lambda x: (x[0], -x[2], x[3]))
 
 def countMolecularBarcodesClustersHierarchical(reads, allowed_mismatches, mc_start_position,
-                                               mc_end_position, min_cluster_size, method = "single"):
+                                               mc_end_position, min_cluster_size, method_ = "single"):
     """
     :param reads the list of reads to be searched for clusters in the form of tuple (read_name, sequence, quality)
     :param allowed_mismatches how much distance we allow between clusters
@@ -61,13 +61,19 @@ def countMolecularBarcodesClustersHierarchical(reads, allowed_mismatches, mc_sta
     This approach finds clusters using hierarchical clustering
     """
     molecular_barcodes = extractMolecularBarcodes(reads, mc_start_position, mc_end_position)
+    # linkage will not work for distance matrices of 1x1 or 2x2 so for these rare cases
+    # we use the naive clustering
+    if len(molecular_barcodes) <= 2:
+        return countMolecularBarcodesClustersNaive(reads,allowed_mismatches, mc_start_position,
+                                                    mc_end_position, min_cluster_size)
     def d(coord):
         i,j = coord
         return hamming_distance(molecular_barcodes[i][0], molecular_barcodes[j][0])
+    
     indices = np.triu_indices(len(molecular_barcodes), 1)
     distance_matrix = np.apply_along_axis(d, 0, indices)
-    linkage = scipy.cluster.hierarchy.linkage(distance_matrix, method=method)
-    flat_clusters = scipy.cluster.hierarchy.fcluster(linkage, allowed_mismatches, criterion='distance')
+    linkage_cluster = linkage(distance_matrix, method=method_)
+    flat_clusters = fcluster(linkage_cluster, allowed_mismatches, criterion='distance')
 
     # Retrieve the original reads from the clusters found
     clusters = []