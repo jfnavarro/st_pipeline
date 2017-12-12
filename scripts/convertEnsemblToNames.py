3337ec1d339adb8df9e2e0917cd2ebfe5b4dda15
elhb <eriklhb@gmail.com>
2017-12-12 17:57:03 +0100

    adding genes_replaced dict to track already replaced gene names

--- scripts/convertEnsemblToNames.py
+++ scripts/convertEnsemblToNames.py
@@ -51,10 +51,35 @@ def main(st_data_file, annotation, output_file):
         if count == 1: break
         sys.stdout.write("Warning, gene_id {} was found {} times in the input matrix.\n".format(gene_id, count))
 
+    genes_replaced = dict()
+
     adjustedList = list()
     for gene_id in st_data.columns:
         try:
             gene_name = gene_map[gene_id]
+
+            # check if the gene_name has been "used" before
+            if gene_name not in genes_replaced:
+                genes_replaced[gene_name] = [gene_id]
+
+            else: # this means the gene name will be duplicated in the output
+                message = 'Warning, gene_name {} is already present in converted set.'.format(gene_name)
+                genes_replaced[gene_name].append(gene_id)
+
+                # check if the corresponding gene id was duplicated in the ST data input file,
+                # in this case it will be dupluicated in output as well
+                if gene_ids_counter[gene_id] > 1:
+                    message += ' The corresponding gene_id was present {} times in the input ST data file.'.format( gene_ids_counter[gene_id] )
+
+                # On the otherhand if the gene id was unique in the ST data input there must >1 ids mapping to the same name
+                # in this case the original (unique) gene id will be used instead of the gene name
+                else:
+                    message += ' The gene_id {} was unique in the input ST data file.'.format( gene_id )
+                    message += ' The following already converted gene_ids also map to the gene_name {}: {}.'.format( gene_name, ', '.join(genes_replaced[gene_name][:-1]) )
+                    message += ' The original gene_id ({}) will be used in the output file for the entry.'.format(gene_id)
+                    gene_name = gene_id
+
+                sys.stdout.write('{}\n'.format(message))
 
             if gene_name in non_unique_gene_names:
                 non_unique_gene_names[gene_name][gene_id] = True
