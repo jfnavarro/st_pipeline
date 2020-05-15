b0d021c2058b178ed47afeed6ebb50000f8ad999
jfnavarro <jc.fernandez.navarro@gmail.com>
2020-05-15 14:20:49 +0200

    Update docs and tests

--- tests/pipeline_run_test.py
+++ tests/pipeline_run_test.py
@@ -182,13 +182,13 @@ class TestPipeline(unittest.TestCase):
         
         # Verify that the stats are correct
         counts_table = pd.read_table(datafile, sep="\t", header=0, index_col=0)
-        self.assertTrue(np.sum(counts_table.values, dtype=np.int32) == 5773, "ST data incorrect stats")
-        self.assertTrue(len(counts_table.columns) == 641, "ST data incorrect stats")
+        self.assertTrue(np.sum(counts_table.values, dtype=np.int32) == 5684, "ST data incorrect stats")
+        self.assertTrue(len(counts_table.columns) == 614, "ST data incorrect stats")
         aggregated_spot_counts = counts_table.sum(axis=1).values
         aggregated_gene_counts = (counts_table != 0).sum(axis=1).values
-        self.assertTrue(aggregated_gene_counts.max() == 77, "ST data incorrect stats")
+        self.assertTrue(aggregated_gene_counts.max() == 75, "ST data incorrect stats")
         self.assertTrue(aggregated_gene_counts.min() == 1, "ST data incorrect stats")
-        self.assertTrue(aggregated_spot_counts.max() == 158, "ST data incorrect stats")
+        self.assertTrue(aggregated_spot_counts.max() == 156, "ST data incorrect stats")
         self.assertTrue(aggregated_spot_counts.min() == 1, "ST data incorrect stats")
         
     def test_normal_run(self):
